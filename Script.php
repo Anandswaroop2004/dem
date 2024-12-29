ce2f608141f975267a33ff931bb486f046610d81
AnandSwaroop <swaroopanand2002@gmail.com>
2024-12-29 17:51:37 +0530

    first commit

---
+++ Script.php
@@ -0,0 +1,10 @@
+// document.getElementById('downloadBtn').addEventListener('click', function() {
+//     // Path to the PDF file you want to allow the user to download
+//     var pdfUrl = 'https://p-def7.pcloud.com/D4ZAFGJ0GZ2LfY4h7ZtMFM7ZZHLyzXkZ2ZZKcJZkZD1eZlYZO4Z54ZmjD05Z98nUNPY1M4QuNS6v4IxPNhSfXqWV/Anand%20Certificate.pdf'; // Replace with your PDF file's URL
+
+//     // Trigger the download
+//     var link = document.createElement('a');
+//     link.href = pdfUrl;
+//     link.download = 'course_certificate.pdf';  // Specify the default filename for download
+//     link.click();
+// });
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  </head>
+  <body>
+    <center
+      style="
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      "
+    >
+      <a
+        href="https://p-def7.pcloud.com/D4ZAFGJ0GZ2LfY4h7ZZZEmjzXkZ2ZZNsHZkZD1eZO4ZlYZ54ZmjD05ZxedY6nGAi2bT5E5fgcO5ykIaeNXX/NPTEL24MG96S551800315.pdf"
+        target="_blank" download
+        style="
+          color: white;
+          background-color: #3070bf;
+          padding: 10px 20px;
+          border-radius: 8px;
+          text-decoration: none;
+          display: inline-block;
+          width: 156px;
+        "
+        >Course Certificate</a
+      >
+        </center>
+  </body>
+</html>
